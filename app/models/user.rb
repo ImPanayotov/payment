bf5ff4f8117bc6d0b76115ea1d0ac18af2bcd15a
impanayotov <i.m.panayotov@gmail.com>
2023-03-12 16:17:00 +0200

    Add admin model and roles to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLES = %i[admin].freeze
+
+  validates :role, presence: true, inclusion: {
+    in: ROLES.map(&:to_s),
+    allow_nil: true
+  }
+
+  ROLES.each do |role|
+    define_method "#{role}_role?" do
+      role?(role)
+    end
+  end
+
+  def role?(name)
+    role == name.to_s
+  end
 end
