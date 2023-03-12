b168ed50ddbd75e4b972bf59d70e18affb7d28a4
impanayotov <i.m.panayotov@gmail.com>
2023-03-12 11:01:54 +0200

    Add `devise` and configure it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.rails_logger = true
   end
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
