9c224bc790cb5a31642f7903a71eeb7e5962d495
impanayotov <i.m.panayotov@gmail.com>
2023-03-30 21:16:36 +0300

    Update transaction removal job
    to destroy hourky every task older than one hour

--- app/workers/txn_hourly_removal_worker.rb
+++ app/workers/txn_hourly_removal_worker.rb
@@ -2,7 +2,11 @@ class TxnHourlyRemovalWorker
   include Sidekiq::Worker
 
   def perform
-    Transaction.where.not(follow_transaction_id: nil).destroy_all
-    Transaction.destroy_all
+    Transaction.where("created_at <= ?", 1.minute.ago)
+               .where.not(follow_transaction_id: nil)
+               .destroy_all
+
+    Transaction.where("created_at <= ?", 1.minute.ago)
+               .destroy_all
   end
 end
