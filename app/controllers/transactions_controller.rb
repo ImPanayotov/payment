8dae4705aa67215cd3a3d660c338d82d0e27ed7f
impanayotov <i.m.panayotov@gmail.com>
2023-03-31 16:18:29 +0300

    Authorize merchants and transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,6 +2,8 @@ class TransactionsController < ApplicationController
   def index
     transactions = Transaction.order(id: :desc)
 
+    authorize(transactions)
+
     render 'transactions/index',
            locals: { transactions: }
   end
