7d4cb03a015cc87e4b36272becb7a4ab7fd4fe79
Ivo M. Panayotov <i.m.panayotov@gmail.com>
2023-04-01 20:56:39 +0300

    Update txn_hourly_removal_worker.rb

--- app/workers/txn_hourly_removal_worker.rb
+++ app/workers/txn_hourly_removal_worker.rb
@@ -2,11 +2,11 @@ class TxnHourlyRemovalWorker
   include Sidekiq::Worker
 
   def perform
-    Transaction.where('created_at <= ?', 1.minute.ago)
+    Transaction.where('created_at <= ?', 1.hour.ago)
                .where.not(follow_transaction_id: nil)
                .destroy_all
 
-    Transaction.where('created_at <= ?', 1.minute.ago)
+    Transaction.where('created_at <= ?', 1.hour.ago)
                .destroy_all
   end
 end
